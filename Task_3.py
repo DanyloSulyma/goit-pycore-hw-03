625f76c7dc0950095f2ff3533fc8ca60c68e67d2
DANYLO SULYMA <Danulo007@gmail.com>
2026-02-07 13:07:04 +0100

    last change

--- Task_3.py
+++ Task_3.py
@@ -1,4 +1,13 @@
 import re
 
 def normalize_phone(phone_number):
-    pass
+    pattern = r"[^\d+]"
+    replacement = ""
+    normalized_number = re.sub(pattern, replacement, phone_number)
+    if normalized_number.startswith("380"):
+        normalized_number = "+" + normalized_number
+    elif normalized_number.startswith("0"):
+        normalized_number = "+38" + normalized_number
+    elif normalized_number.startswith("80"):
+        normalized_number = "+3" + normalized_number
+    return normalized_number
